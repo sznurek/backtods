8d7461daf727688f7f9d055929a4dc6c767a6d65
Łukasz Dąbek <sznurek@gmail.com>
2013-09-10 11:48:30 +0200

    First proven version.

--- ds.v
+++ ds.v
@@ -22,10 +22,10 @@ Inductive CrootValid : Croot -> Prop :=
 | CrootValid_exp : forall e:Cexp, CexpValid e nil -> CrootValid e
 with CexpValid : Cexp -> list var -> Prop :=
 | CexpValid_cont : forall (ksi:list var) (t:Ctriv), CtrivValid t ksi nil -> CexpValid (Cexp_cont t) ksi
-| CexpValid_app  : forall (ksi0 ksi1 ksi2:list var) (v:var) (t0 t1:Ctriv) (e:Cexp),
+| CexpValid_app  : forall (ksi0 ksi1 ksi2:list var) (t0 t1:Ctriv) (e:Cexp),
                      CtrivValid t1 ksi0 ksi1 -> CtrivValid t0 ksi1 ksi2 ->
-                     CexpValid e (v::ksi2) ->
-                     CexpValid (Cexp_app t0 t1 v e) ksi0
+                     CexpValid e (0::ksi2) ->
+                     CexpValid (Cexp_app t0 t1 0 e) ksi0
 with CtrivValid : Ctriv -> list var -> list var -> Prop :=
 | CtrivValid_vvar : forall (ksi:list var) (v:var), CtrivValid Ctriv_vvar (v::ksi) ksi
 | CtrivValid_var  : forall (ksi:list var) (x:var), CtrivValid (Ctriv_var x) ksi ksi
@@ -97,7 +97,7 @@ apply (Droot_mut (fun r => CrootValid (cps_transform r))
 intros; unfold good_continuation in *; eauto; simpl; intuition; eauto.
 Qed.
 
-Definition mold (e:Dexp) (rest:Cexp) : Cexp :=
+Definition mold (rest:Cexp) (e:Dexp) : Cexp :=
   cps_exp_transform e (fun t => rest).
 
 Lemma length_zero_is_nil :
@@ -120,11 +120,36 @@ Definition is_app_list (es:list Dexp) : Prop :=
   Forall is_app es.
 
 Lemma app_produces_vvar :
-  forall (e:Dexp), is_app e -> cps_exp_transform e (fun t => Cexp_cont t) =
-                               cps_exp_transform e (fun _ => Cexp_cont Ctriv_vvar).
+  forall (e:Dexp) (k:Cont),
+    is_app e -> cps_exp_transform e k =
+                cps_exp_transform e (fun _ => k Ctriv_vvar).
 Proof.
 induction e; intros; simpl in *; eauto.
 inversion H.
+Qed.
+
+Lemma has_two_elements :
+  forall {A:Type} (es:list A) (n:nat), length es = S (S n) ->
+         exists (a b:A), exists es':list A, es = a::b::es'.
+Proof.
+intros.
+destruct es; simpl in *; eauto.
+inversion H.
+destruct es.
+simpl in H; inversion H.
+destruct es.
+exists a.
+exists a0.
+exists nil.
+trivial.
+exists a; exists a0; exists (a1::es); trivial.
+Qed.
+
+Lemma equal_arguments :
+  forall {A B:Type} (f:A -> B) (x y:A), x = y -> f x = f y.
+Proof.
+intros; eauto.
+rewrite H; auto.
 Qed.
 
 Theorem cps_inverse_exists :
@@ -132,14 +157,14 @@ Theorem cps_inverse_exists :
 Proof.
 apply (CrootValid_mut
          (fun r rv => exists r':Droot, cps_transform r' = r)
-         (fun t l0 l1 tv => (exists e, l0 = e::l1) \/
+         (fun t l0 l1 tv => ((exists e, l0 = e::l1) /\ t = Ctriv_vvar) \/
                             (exists e, cps_triv_transform e = t /\ l0 = l1))
          (fun e l ev => forall (es:list Dexp),
            length l = length es -> is_app_list es ->
            exists e':Dexp,
              cps_exp_transform e' (fun t => Cexp_cont t) =
-             fold_right mold e es))
-;intros; eauto; simpl in *; intuition.
+             fold_left mold es e))
+;intros; eauto; simpl in *.
 
 specialize (H nil).
 simpl in H.
@@ -158,15 +183,20 @@ destruct H.
 exists (Dtriv_lam x x0).
 simpl; rewrite H; auto.
 
-destruct H2; subst.
-destruct es.
-simpl in H0; inversion H0.
+destruct H.
+destruct H.
+destruct H.
+subst.
 simpl in H0.
+symmetry in H0.
+destruct es.
+inversion H0.
+
 assert (es = nil).
 apply length_zero_is_nil.
 replace (length es) with (pred (S (length es))).
 replace 0 with (pred (S 0)).
-rewrite H0; simpl; trivial.
+simpl in H0; rewrite H0; simpl; trivial.
 simpl; trivial.
 simpl; trivial.
 subst.
@@ -177,10 +207,122 @@ apply app_produces_vvar.
 unfold is_app_list in H1.
 inversion H1; subst; auto.
 
-destruct H2; destruct H; subst.
+destruct H; destruct H; subst.
 exists x.
 unfold mold; simpl.
 symmetry in H0.
 specialize (length_zero_is_nil es H0); intros; subst; simpl; auto.
 
-Admitted.
+intuition.
+destruct H; destruct H0; subst; simpl in *.
+symmetry in H2.
+specialize (has_two_elements es (length ksi2) H2); intros.
+destruct H.
+destruct H.
+destruct H.
+subst.
+specialize (H1 (Dexp_app x2 x1::x3)).
+simpl in H1.
+simpl in H2.
+inversion H2.
+rewrite H0 in H1.
+specialize (H1 eq_refl).
+
+assert (is_app_list (Dexp_app x2 x1 :: x3)).
+constructor.
+unfold is_app; simpl; auto.
+inversion H3; subst.
+inversion H6; subst.
+trivial.
+
+specialize (H1 H).
+destruct H1.
+exists x4.
+simpl.
+rewrite H1.
+unfold mold.
+simpl.
+
+rewrite app_produces_vvar.
+apply equal_arguments.
+apply equal_arguments.
+
+apply functional_extensionality; intros.
+rewrite app_produces_vvar.
+apply equal_arguments.
+apply functional_extensionality; intros.
+trivial.
+
+
+inversion H3; trivial.
+inversion H3; subst.
+inversion H7; auto.
+
+destruct H4.
+destruct H0.
+destruct H.
+subst.
+
+simpl in H2.
+destruct es.
+inversion H2.
+simpl in H2.
+specialize (H1 (Dexp_app x d::es)).
+simpl in H1.
+rewrite H2 in H1.
+specialize (H1 eq_refl).
+
+assert (is_app_list (Dexp_app x d :: es)).
+constructor; simpl; auto.
+inversion H3; subst; auto.
+specialize (H1 H).
+destruct H1.
+exists x1.
+simpl.
+rewrite H0.
+apply equal_arguments.
+unfold mold; simpl.
+apply app_produces_vvar.
+inversion H3; trivial.
+
+
+
+
+destruct H4.
+destruct H0.
+destruct H.
+subst.
+
+simpl in H2.
+destruct es.
+inversion H2.
+simpl in H2.
+specialize (H1 (Dexp_app d x::es)).
+simpl in H1.
+rewrite H2 in H1.
+specialize (H1 eq_refl).
+
+assert (is_app_list (Dexp_app d x :: es)).
+constructor; simpl; auto.
+inversion H3; subst; auto.
+specialize (H1 H).
+destruct H1.
+exists x1.
+simpl.
+rewrite H0.
+apply equal_arguments.
+unfold mold; simpl.
+apply app_produces_vvar.
+inversion H3; trivial.
+
+
+destruct H4, H; destruct H0, H; subst.
+specialize (H1 (Dexp_app x0 x::es)).
+simpl in H1; rewrite H2 in H1; specialize (H1 eq_refl).
+
+assert (is_app_list (Dexp_app x0 x :: es)).
+constructor; simpl; auto.
+inversion H3; subst; auto.
+Qed.
+
+
